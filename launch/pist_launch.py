a29272042e2ef6d04bbba876139ed1abf799ae93
akif-dlk <akifdlk58svs@gmail.com>
2025-02-28 00:41:08 +0300

    lidar açısı değiştirildi topic ismi değişti odometry estimation silindi

--- launch/pist_launch.py
+++ launch/pist_launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
 		'/model/Evata/pose@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
 		'/clock@rosgraph_msgs/msg/Clock@ignition.msgs.Clock',
 		'/camera/rgb@sensor_msgs/msg/Image@ignition.msgs.Image',
-		'/lidar/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+		'/lidar@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
 		'/odom@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
 		'/depth_camera/zed/image@sensor_msgs/msg/Image@ignition.msgs.Image',
 		'/depth_camera/zed/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloud',
