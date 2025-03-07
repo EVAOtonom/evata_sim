6662c248cb0f0f5ad1d802ed096db310f439ebda
Oğuzcan kurt <99121324+oguzcankurt@users.noreply.github.com>
2025-03-07 11:27:29 +0300

    Add files via upload
    navigasyon  dosyaları düzenlendi

--- launch/navigation2.launch.py
+++ launch/navigation2.launch.py
@@ -1,4 +1,3 @@
-
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -9,54 +8,67 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+   
+   
+     # Mevcut çalışma dizinini al
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+   
+    # Harita ve parametre dosyalarının dinamik yollarını al
+    src_dir = dir_path.split('/install')[0]  
     map_dir = LaunchConfiguration(
         'map',
-    	default="/home/oguzcan/otonom_ws/src/evata_sim/map/my_map.yaml")
-    param_file_name = "evata.yaml"
+        default=os.path.join(src_dir,"src","evata_sim","navigasyon", 'map', 'my_map.yaml')
+    )
     param_dir = LaunchConfiguration(
         'params_file',
-        default="/home/oguzcan/otonom_ws/src/evata_sim/params/evata.yaml")
-         
+        default=os.path.join(src_dir,"src","evata_sim","navigasyon", 'params', 'evata.yaml')
+    )
 
+    # nav2_launch_file_dir ve rviz_config_dir yolları
     nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
-
     rviz_config_dir = os.path.join(
         get_package_share_directory('nav2_bringup'),
         'rviz',
-        'nav2_default_view.rviz')
+        'nav2_default_view.rviz'
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'map',
             default_value=map_dir,
-            description='Full path to map file to load'),
-
+            description='Full path to map file to load'
+        ),
         DeclareLaunchArgument(
             'params_file',
             default_value=param_dir,
-            description='Full path to param file to load'),
-
+            description='Full path to param file to load'
+        ),
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='true',
-            description='Use simulation (Gazebo) clock if true'),
+            description='Use simulation (Gazebo) clock if true'
+        ),
 
+        # NAV2 launch dosyasını dahil et
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
             launch_arguments={
                 'map': map_dir,
                 'use_sim_time': use_sim_time,
-                'params_file': param_dir}.items(),
+                'params_file': param_dir
+            }.items(),
         ),
 
+        # RViz'i başlat
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+            output='screen'
+        ),
     ])
+
--- launch/robot_state_publisher.launch.py
+++ launch/robot_state_publisher.launch.py
@@ -1,4 +1,3 @@
-
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -7,22 +6,28 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import xacro
 
-
-
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
-    sdf = "/home/oguzcan/otonom_ws/src/evata_sim/pist_world/models/Evata/model.sdf"
- 
-    doc = xacro.parse(open(sdf))
+     # Mevcut çalışma dizinini al
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    src_dir = dir_path.split('/install')[0]  # install kısmını çıkar
+    
+    # model dosyasının yolu
+    sdf_path = os.path.join(src_dir, "src","evata_sim",'pist_world', 'models', 'Evata', 'model.sdf')
+
+    # Xacro dosyasını oku ve işleme yap
+    doc = xacro.parse(open(sdf_path))
     xacro.process_doc(doc)
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='true',
-            description='Use simulation (Gazebo) clock if true'),
+            description='Use simulation (Gazebo) clock if true'
+        ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -31,3 +36,4 @@ def generate_launch_description():
             parameters=[{'use_sim_time': use_sim_time,
                          'robot_description': doc.toxml()}]),
     ])
+
