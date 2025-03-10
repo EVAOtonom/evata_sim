214ac19dae34a953260ba249c792d4cd0da00651
Sefa-Kara <mustafasefakara7@gmail.com>
2025-03-10 15:00:51 +0300

    haritalama fixes

--- launch/haritalama.launch.py
+++ launch/haritalama.launch.py
@@ -7,6 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
@@ -59,11 +60,61 @@ def generate_launch_description():
     evata_sim_share_dir = get_package_share_directory('evata_sim')
 
     launch_file_dir = os.path.join(evata_sim_share_dir, 'launch')
+    
+    
+    
+    
+    bridge = Node(
+        package='ros_ign_bridge',
+        executable='parameter_bridge',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[
+                # Velocity command (ROS2 -> IGN)
+                '/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
+                # Odometry (IGN -> ROS2)
+                '/odom@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
+                # TF (IGN -> ROS2)
+                '/odom/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
+                # Clock (IGN -> ROS2)
+                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+                # Joint states (IGN -> ROS2)
+                '/joint_states@sensor_msgs/msg/JointState[ignition.msgs.Model',
+                # Lidar (IGN -> ROS2)
+                '/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+                '/scan/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+                # IMU (IGN -> ROS2)
+                '/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
+                # Camera (IGN -> ROS2)
+                '/camera/rgb/image_raw@sensor_msgs/msg/Image[ignition.msgs.Image',
+                '/camera/rgb/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+                ],
+        remappings=[
+            ("/odom/tf", "tf"),
+        ],
+        output='screen'
+    )
+
+    map_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'odom'])
+    
+    sdf = os.path.join(
+        get_package_share_directory('turtlebot3'),
+        'models', 'turtlebot3', 'model.sdf')
+    
+    doc = xacro.parse(open(sdf))
+    xacro.process_doc(doc)
+
+
 
     return LaunchDescription([
         ign_resource_path,
         ignition_spawn_entity,
         ignition_spawn_world,
+        bridge,
+        map_static_tf,
         
         # Ign Gazebo Launch
         IncludeLaunchDescription(
@@ -84,6 +135,8 @@ def generate_launch_description():
             default_value=world_name,
             description='World name'
         ),
+        
+
 
         
     ])
