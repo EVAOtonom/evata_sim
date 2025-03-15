b488a2aa7f1de43ea1b96ff238526daa5c8956c1
Sefa-Kara <mustafasefakara7@gmail.com>
2025-03-15 13:21:36 +0300

    Minor Fixes

--- evata_sim/utils/utils.py
+++ evata_sim/utils/utils.py
@@ -66,7 +66,7 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, [0,255,255], thickness=2, lineType=cv2.LINE_AA)
+    #cv2.rectangle(img, c1, c2, [0,255,255], thickness=2, lineType=cv2.LINE_AA)
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
@@ -201,7 +201,7 @@ def show_seg_result(img, result, palette=None,is_demo=False):
     else:
         color_area = np.zeros((result[0].shape[0], result[0].shape[1], 3), dtype=np.uint8)
         
-        color_area[result[0] == 1] = [0, 255, 0]
+        #color_area[result[0] == 1] = [0, 255, 0]
         color_area[result[1] ==1] = [255, 0, 0]
         color_seg = color_area
 
